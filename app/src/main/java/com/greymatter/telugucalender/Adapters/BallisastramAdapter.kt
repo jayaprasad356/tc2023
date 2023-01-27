10212073ba5179e99268a8c68837e22b82269b2c
jayaprasad356 <jayaprasad356@gmail.com>
2023-01-28 00:49:54 +0530

    api integrated

--- app/src/main/java/com/greymatter/telugucalender/Adapters/BallisastramAdapter.kt
+++ app/src/main/java/com/greymatter/telugucalender/Adapters/BallisastramAdapter.kt
@@ -1,10 +1,13 @@
 package com.greymatter.telugucalender.Adapters
 
 import android.app.Activity
+import android.graphics.Typeface
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.webkit.WebView
+import android.webkit.WebViewClient
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.greymatter.telugucalender.Model.Ballisastram
@@ -30,10 +33,30 @@ class BallisastramAdapter(val activity: Activity, ballisastram: ArrayList<Ballis
         val holder = holderParent as ExploreItemHolder
         val ballisastram: Ballisastram = ballisastram[position]
 
-        holder.tvTitle.setText(ballisastram.title)
         holder.tvdescription.setVerticalScrollBarEnabled(true)
 
-        holder.tvdescription.loadDataWithBaseURL("", ballisastram.description, "text/html", "UTF-8", "")
+
+
+        holder.tvdescription.loadDataWithBaseURL("file:///android_asset/fonts/", "<html>\n" + "<link rel='stylesheet' type='text/css' href='file:///android_asset/fonts/nats.css'>"+
+                "<style>\n" +
+                "body {\n" +
+                "    font-family: 'nats';\n" +
+                "}\n" +
+                "\n" +
+                "</style>"+
+                "<body>\n" +
+                ballisastram.description+
+                "</body>\n" +
+                "</html>"
+            , "text/html", "UTF-8", "")
+        holder.tvdescription.getSettings().setJavaScriptEnabled(true);
+        holder.tvdescription.getSettings().setLoadWithOverviewMode(true);
+
+        holder.tvTitle.setText(ballisastram.title)
+
+
+
+
     }
 
     override fun getItemCount(): Int {
